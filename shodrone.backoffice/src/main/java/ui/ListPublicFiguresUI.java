7ec7c9b6d3f4f4c296d0d9cadeedfa46467e2339
paulo-mendes <1231498@isep.ipp.pt>
2025-05-02 16:31:44 +0100

    US 231 - Update ListPublicFigures to page from a starting page - Updated show paged elements list Related to: #1/#46/#69

--- shodrone.backoffice/src/main/java/ui/ListPublicFiguresUI.java
+++ shodrone.backoffice/src/main/java/ui/ListPublicFiguresUI.java
@@ -12,21 +12,26 @@ public class ListPublicFiguresUI implements Runnable {
 
     @Override
     public void run() {
-        Utils.printCenteredTitle("Not implemented yet");
-
         Utils.printCenteredTitle("List Public Figures");
 
         List<Figure> allPublicFigures = listPublicFiguresController.listPublicFigures();
         if (!allPublicFigures.isEmpty()) {
 
             boolean page = Utils.confirm("Do you want to select a page number? (y/n)");
-            int newpage = page ? Utils.readIntegerFromConsole("Enter the page number: ") : 0;
+            int newpage = page ? Utils.readIntegerFromConsole("Enter the page number: ") : 1;
+
+            Utils.dropLines(1);
 
             boolean pageSize = Utils.confirm("Do you want to select a page size? (y/n)");
             int newPageSize = pageSize ? Utils.readIntegerFromConsole("Enter the page size: ") : 20;
 
-            Utils.showPagedElementList(allPublicFigures, newPageSize, "List Public Figures");
+            Utils.dropLines(2);
 
+            Utils.showPagedElementListByStartingPage(allPublicFigures, newpage, newPageSize, "List Public Figures");
+
+            Utils.dropLines(1);
+            System.out.println("....... Paused to read press ENTER key to continue ........");
+            new java.util.Scanner(System.in).nextLine();
         } else {
             Utils.printFailMessage("No figures in the system yet.");
         }
