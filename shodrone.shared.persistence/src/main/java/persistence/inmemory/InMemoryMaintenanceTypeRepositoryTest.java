3faddb10a11b359f41f3bf93569a653d33589bc9
Francisco Santos <1230564@isep.ipp.pt>
2025-06-02 18:10:43 +0100

    pom.xml updates (all)

--- shodrone.shared.persistence/src/main/java/persistence/inmemory/InMemoryMaintenanceTypeRepositoryTest.java
+++ shodrone.shared.persistence/src/main/java/persistence/inmemory/InMemoryMaintenanceTypeRepositoryTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.testng.AssertJUnit.assertTrue;
+
 
 class InMemoryMaintenanceTypeRepositoryTest {
 
