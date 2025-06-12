b51c16f2793eabd50c0fb693d5db4ae93bcaa271
Jorge Ubaldo Laptob <1231274@isep.ipp.pt>
2025-06-12 18:18:53 +0100

    Documentation for Server App JavaDoc for mine USs

--- shodrone.customer_app_server/src/main/java/controller/Representative/FindCustomerOfRepresentativeController.java
+++ shodrone.customer_app_server/src/main/java/controller/Representative/FindCustomerOfRepresentativeController.java
@@ -1,7 +1,6 @@
 package controller.Representative;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import domain.entity.Costumer;
 import domain.valueObjects.NIF;
@@ -18,32 +17,52 @@ import static more.ColorfulOutput.ANSI_BRIGHT_PURPLE;
 import static more.ColorfulOutput.ANSI_RESET;
 import static more.TextEffects.BOLD;
 
+/**
+ * Server-side controller responsible for retrieving the {@link domain.entity.Costumer}
+ * associated with a given Customer Representative.
+ * <p>
+ * This controller handles the protocol action "FindCustomerOfRepresentative", which expects
+ * the client's email and returns the {@link NIF} of the associated customer (if one exists).
+ */
 public class FindCustomerOfRepresentativeController {
 
-    public static void getCustomerOfRepresentativeAction(BufferedReader in, PrintWriter out,Gson gson) {
+    /**
+     * Executes the "FindCustomerOfRepresentative" server action.
+     * <p>
+     * The expected communication protocol is:
+     * <ol>
+     *     <li>Read a {@link ObjectDTO}&lt;String&gt; containing the representative's email.</li>
+     *     <li>Search for a {@link Costumer} linked to that representative via {@link RepositoryProvider}.</li>
+     *     <li>Return a {@link ObjectDTO}&lt;NIF&gt;: the customer's NIF if found, or null otherwise.</li>
+     * </ol>
+     *
+     * @param in    BufferedReader to receive the client's email request.
+     * @param out   PrintWriter to send the NIF response back to the client.
+     * @param gson  Gson instance used for JSON (de)serialization.
+     */
+    public static void getCustomerOfRepresentativeAction(BufferedReader in, PrintWriter out, Gson gson) {
         try {
-
+            // Read email from the client
             String receivedEmail = in.readLine();
             Type objectDTOType = new TypeToken<ObjectDTO<String>>() {}.getType();
             ObjectDTO<String> objectDTO = gson.fromJson(receivedEmail, objectDTOType);
-
             String email = objectDTO.getObject();
 
+            // Try to find a customer linked to this representative
             Optional<Costumer> optionalCustomer = RepositoryProvider.customerRepresentativeRepository().getAssociatedCustomer(email);
 
-            if(optionalCustomer.isPresent()) {
-                NIF nif =  optionalCustomer.get().nif();
-                ObjectDTO<NIF> objectDTOToSend = ObjectDTO.of(nif);
-                String objectDTOJsonToSend = gson.toJson(objectDTOToSend);
-                out.println(objectDTOJsonToSend);
-            }else{
-                ObjectDTO<NIF> objectDTOToSend = ObjectDTO.of(null);
-                String objectDTOJsonToSend = gson.toJson(objectDTOToSend);
-                out.println(objectDTOJsonToSend);
-            }
+            // Prepare response DTO with the customer's NIF or null
+            ObjectDTO<NIF> objectDTOToSend = optionalCustomer
+                    .map(customer -> ObjectDTO.of(customer.nif()))
+                    .orElse(ObjectDTO.of(null));
 
+            // Serialize and send response
+            String objectDTOJsonToSend = gson.toJson(objectDTOToSend);
+            out.println(objectDTOJsonToSend);
 
-            System.out.printf("%s%s❕SERVER ACTION FINISHED: Get Customer of Representative -> [%s] %s%n",ANSI_BRIGHT_PURPLE,BOLD,email,ANSI_RESET);
+            // Log server-side action
+            System.out.printf("%s%s❕SERVER ACTION FINISHED: Get Customer of Representative -> [%s] %s%n",
+                    ANSI_BRIGHT_PURPLE, BOLD, email, ANSI_RESET);
 
         } catch (IOException e) {
             System.out.println("Client error: " + e.getMessage());
--- shodrone.customer_app_server/src/main/java/controller/Show/FindShow4CustomerController.java
+++ shodrone.customer_app_server/src/main/java/controller/Show/FindShow4CustomerController.java
@@ -21,53 +21,81 @@ import static more.ColorfulOutput.ANSI_BRIGHT_PURPLE;
 import static more.ColorfulOutput.ANSI_RESET;
 import static more.TextEffects.BOLD;
 
+/**
+ * Controller class responsible for handling the server-side logic
+ * for retrieving all {@link Show} entities associated with a given {@link Costumer}.
+ * <p>
+ * This controller is triggered by a remote client request and uses the
+ * {@link RepositoryProvider} to access the persistent storage.
+ * </p>
+ * <p>
+ * The result is returned in the form of {@link ShowDTO} objects
+ * serialized with Gson and sent over the network stream.
+ * </p>
+ */
 public class FindShow4CustomerController {
 
+    /**
+     * Static method that executes the server-side action for the "FindShow4Customer" protocol.
+     * <p>
+     * The method follows this sequence:
+     * <ol>
+     *     <li>Reads a {@code NIF} (wrapped in {@code ObjectDTO}) sent by the client.</li>
+     *     <li>Validates whether the customer exists in the repository using that NIF.</li>
+     *     <li>If the customer exists, retrieves all their associated shows.</li>
+     *     <li>Returns the number of shows and each show serialized as {@code ShowDTO}.</li>
+     *     <li>If no shows exist, returns 0. If customer doesn't exist, returns -1.</li>
+     * </ol>
+     * </p>
+     *
+     * @param in   Input stream from the client (to read the NIF).
+     * @param out  Output stream to the client (to send show data).
+     * @param gson Gson instance used for serialization/deserialization.
+     */
     public static void getShow4CustomerAction(BufferedReader in, PrintWriter out, Gson gson) {
         try {
-
+            // Step 1: Receive NIF from client
             String receivedEmail = in.readLine();
             Type objectDTOType = new TypeToken<ObjectDTO<NIF>>() {}.getType();
             ObjectDTO<NIF> objectDTO = gson.fromJson(receivedEmail, objectDTOType);
-
             NIF nifCustomer = objectDTO.getObject();
 
+            // Step 2: Find customer by NIF
             Optional<Costumer> customerToAssociatedNIF = RepositoryProvider.costumerRepository().findByNIF(nifCustomer);
 
-            if(customerToAssociatedNIF.isPresent()){
+            if (customerToAssociatedNIF.isPresent()) {
+                // Step 3: Find shows for that customer
                 Optional<List<Show>> listOfShowsOptional = RepositoryProvider.showRepository().findByCostumer(customerToAssociatedNIF.get());
 
-
-                if(listOfShowsOptional.isPresent()) {
+                if (listOfShowsOptional.isPresent()) {
                     List<Show> listOfShows = listOfShowsOptional.get();
                     int numShows = listOfShows.size();
 
+                    // Step 4: Send number of shows
                     ObjectDTO<Integer> objectDTOToSendInformative = ObjectDTO.of(numShows);
                     String objectDTOJsonToSendInformative = gson.toJson(objectDTOToSendInformative);
                     out.println(objectDTOJsonToSendInformative);
 
-                    for(int i = 0; i < numShows; i++) {
-                        ShowDTO objectDTOToSend = ShowDTO.fromEntity(listOfShows.get(i));
+                    // Step 5: Send each show as ShowDTO
+                    for (Show show : listOfShows) {
+                        ShowDTO objectDTOToSend = ShowDTO.fromEntity(show);
                         String objectDTOJsonToSend = gson.toJson(objectDTOToSend);
                         out.println(objectDTOJsonToSend);
                     }
-
-                }else{
-                    int numShows = 0;
-
-                    ObjectDTO<Integer> objectDTOToSendInformative = ObjectDTO.of(numShows);
-                    String objectDTOJsonToSendInformative = gson.toJson(objectDTOToSendInformative);
-                    out.println(objectDTOJsonToSendInformative);
+                } else {
+                    // No shows for this customer
+                    ObjectDTO<Integer> objectDTOToSendInformative = ObjectDTO.of(0);
+                    out.println(gson.toJson(objectDTOToSendInformative));
                 }
-
-            }else{
-                int numShows = -1;
-                ObjectDTO<Integer> objectDTOToSendInformative = ObjectDTO.of(numShows);
-                String objectDTOJsonToSendInformative = gson.toJson(objectDTOToSendInformative);
-                out.println(objectDTOJsonToSendInformative);
+            } else {
+                // Customer not found
+                ObjectDTO<Integer> objectDTOToSendInformative = ObjectDTO.of(-1);
+                out.println(gson.toJson(objectDTOToSendInformative));
             }
 
-            System.out.printf("%s%s❕SERVER ACTION FINISHED: Get Show for Customer of Representative -> [%s] %s%n",ANSI_BRIGHT_PURPLE,BOLD,nifCustomer,ANSI_RESET);
+            // Log successful server response
+            System.out.printf("%s%s❕SERVER ACTION FINISHED: Get Show for Customer of Representative -> [%s] %s%n",
+                    ANSI_BRIGHT_PURPLE, BOLD, nifCustomer, ANSI_RESET);
 
         } catch (IOException e) {
             Utils.printFailMessage("Client error: " + e.getMessage());
