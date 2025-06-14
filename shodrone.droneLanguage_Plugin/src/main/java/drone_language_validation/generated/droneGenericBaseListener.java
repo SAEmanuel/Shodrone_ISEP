1c5a69bbcafeead9da3ade716eef87088392b232
Francisco Santos <1230564@isep.ipp.pt>
2025-06-14 19:01:06 +0100

    antlr generated for drone

---
+++ shodrone.droneLanguage_Plugin/src/main/java/drone_language_validation/generated/droneGenericBaseListener.java
@@ -0,0 +1,171 @@
+package drone_language_validation.generated;// Generated from droneGeneric.g4 by ANTLR 4.13.2
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link droneGenericListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class droneGenericBaseListener implements droneGenericListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(droneGenericParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(droneGenericParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterHeader(droneGenericParser.HeaderContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitHeader(droneGenericParser.HeaderContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSection_types(droneGenericParser.Section_typesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSection_types(droneGenericParser.Section_typesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSection_variables(droneGenericParser.Section_variablesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSection_variables(droneGenericParser.Section_variablesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVariable_declaration(droneGenericParser.Variable_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVariable_declaration(droneGenericParser.Variable_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSection_instructions(droneGenericParser.Section_instructionsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSection_instructions(droneGenericParser.Section_instructionsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInstruction(droneGenericParser.InstructionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInstruction(droneGenericParser.InstructionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParam_list(droneGenericParser.Param_listContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParam_list(droneGenericParser.Param_listContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpression(droneGenericParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpression(droneGenericParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVector(droneGenericParser.VectorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVector(droneGenericParser.VectorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterArray_literal(droneGenericParser.Array_literalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitArray_literal(droneGenericParser.Array_literalContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ shodrone.droneLanguage_Plugin/src/main/java/drone_language_validation/generated/droneGenericVisitor.java
@@ -0,0 +1,78 @@
+package drone_language_validation.generated;// Generated from droneGeneric.g4 by ANTLR 4.13.2
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link droneGenericParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface droneGenericVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#program}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProgram(droneGenericParser.ProgramContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#header}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitHeader(droneGenericParser.HeaderContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#section_types}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSection_types(droneGenericParser.Section_typesContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#section_variables}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSection_variables(droneGenericParser.Section_variablesContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#variable_declaration}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVariable_declaration(droneGenericParser.Variable_declarationContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#section_instructions}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSection_instructions(droneGenericParser.Section_instructionsContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#instruction}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInstruction(droneGenericParser.InstructionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#param_list}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParam_list(droneGenericParser.Param_listContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#expression}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpression(droneGenericParser.ExpressionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#vector}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVector(droneGenericParser.VectorContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link droneGenericParser#array_literal}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_literal(droneGenericParser.Array_literalContext ctx);
+}
