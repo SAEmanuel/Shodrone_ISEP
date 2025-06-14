c6eb44701ce11f3ef717f15d1eee5ac214f2b3ad
RomeuZouXu <francpok.zouxu4@gmail.com>
2025-06-14 23:51:11 +0100

    [US344 - Implementation - Issue #141 #221 #225] Changes in logic

--- shodrone.backoffice/src/main/java/Interface/DroneProgramGenerator.java
+++ shodrone.backoffice/src/main/java/Interface/DroneProgramGenerator.java
@@ -2,9 +2,15 @@ package Interface;
 
 import domain.entity.DroneModel;
 import domain.entity.Figure;
+import figure_dsl.validator.FigureValidationPlugin;
 import utils.DslMetadata;
+
+import java.util.List;
+import java.util.Map;
 
 public interface DroneProgramGenerator {
 
-    String generateProgram(Figure figure, DroneModel droneModel, String dslVersion, DslMetadata dslMetadata);
+    public String generateProgram(Figure figure, DroneModel droneModel, String dslVersion,
+                                  DslMetadata dslMetadata, FigureValidationPlugin validator,
+                                  int droneId, Map<Integer, List<int[]>> dronePositions);
 }
--- shodrone.backoffice/src/main/java/controller/drone/ShowProgramGeneratorController.java
+++ shodrone.backoffice/src/main/java/controller/drone/ShowProgramGeneratorController.java
@@ -3,12 +3,11 @@ package controller.drone;
 import Interface.DroneProgramGenerator;
 import drone.DroneProgramsGenerator;
 import domain.entity.*;
+import figure_dsl.validator.FigureValidationPlugin;
 import persistence.RepositoryProvider;
 import utils.DslMetadata;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class ShowProgramGeneratorController {
 
@@ -18,36 +17,64 @@ public class ShowProgramGeneratorController {
         this.generator = new DroneProgramsGenerator();
     }
 
-    /**
-     * Generates the programs for all drones in a given ShowProposal.
-     *
-     * @param showProposal The ShowProposal to generate programs for.
-     */
     public Optional<ShowProposal> generateProgramsForShow(ShowProposal showProposal) {
 
         List<Figure> figures = showProposal.getSequenceFigues();
+        Map<Integer, List<int[]>> dronePositions = new HashMap<>();
+        int droneIdCounter = 0;
 
         for (Map.Entry<DroneModel, Integer> entry : showProposal.getModelsUsed().entrySet()) {
-
             DroneModel droneModel = entry.getKey();
 
             for (Figure figure : figures) {
-
                 Map<String, DslMetadata> dslVersions = figure.dslVersions();
 
                 for (Map.Entry<String, DslMetadata> dslEntry : dslVersions.entrySet()) {
-
                     String dslVersion = dslEntry.getKey();
                     DslMetadata dslMetadata = dslEntry.getValue();
 
-                    String program = generator.generateProgram(figure, droneModel, dslVersion, dslMetadata);
+                    FigureValidationPlugin validator = new FigureValidationPlugin();
+                    validator.setDroneModelName(droneModel.identity());
+                    List<String> validationErrors = validator.validate(dslMetadata.getDslLines());
+
+                    if (!validationErrors.isEmpty()) {
+                        System.out.println("❌ Validation errors in figure '" + figure.name() + "' with drone '" + droneModel.identity() + "':");
+                        validationErrors.forEach(System.out::println);
+                        continue;
+                    }
+
+                    String program = ((DroneProgramsGenerator) generator).generateProgram(
+                            figure, droneModel, dslVersion, dslMetadata, validator, droneIdCounter, dronePositions);
 
                     String key = figure.name().toString() + "_" + droneModel.identity() + "_DSL_" + dslVersion;
                     showProposal.getDroneLanguageSpecifications().put(key, program);
+
+                    droneIdCounter++;
                 }
             }
         }
 
+        List<String> script = generateScriptFromPositions(dronePositions);
+        showProposal.setScript(script);
+
         return RepositoryProvider.showProposalRepository().saveInStore(showProposal);
     }
+
+    private List<String> generateScriptFromPositions(Map<Integer, List<int[]>> dronePositions) {
+        List<String> script = new ArrayList<>();
+        script.add("[" + dronePositions.size() + "]");
+
+        for (Map.Entry<Integer, List<int[]>> entry : dronePositions.entrySet()) {
+            int droneId = entry.getKey();
+            List<int[]> positions = entry.getValue();
+            script.add(droneId + " - 3x4x5");
+
+            for (int[] pos : positions) {
+                script.add(pos[0] + " " + pos[1] + " " + pos[2]);
+            }
+        }
+
+        return script;
+    }
+
 }
